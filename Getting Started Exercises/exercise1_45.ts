ac7876128d3fadd5d604fea3901bc7787d4f18bc
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2023-09-26 12:40:33 +0500

    Getting started exercise completed

--- Getting Started Exercises/exercise1_45.ts
+++ Getting Started Exercises/exercise1_45.ts
@@ -1,5 +1,7 @@
 // 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
 
+// Done
+
 /*********************************************************************************************************************
 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, 
 “Hello Eric, would you like to learn some Python today?”
